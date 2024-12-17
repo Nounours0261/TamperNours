8da670a542a678172c4ce8437fe86c3d11a266c1
Nounours0261 <71218117+Nounours0261@users.noreply.github.com>
2024-12-17 16:42:28 +0100

    Update Kana-chan.user.js

--- Kana-chan.user.js
+++ Kana-chan.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Kana-chan
 // @namespace    Kana-chan
-// @version      Kana-chan
+// @version      1
 // @description  Kana-chan
 // @author       Kana-chan
 // @match        *://*/*
 // @icon         https://media1.tenor.com/m/9e6n8-LUhH8AAAAd/oshi-no-ko-arima.gif
-// @run-at       document-start
+// @run-at       document-idle
+// @require      https://github.com/Nounours0261/TamperNours/raw/refs/heads/main/waitForList.js
 // @grant        GM.setValue
 // @grant        GM.getValue
 // ==/UserScript==
@@ -16,51 +17,44 @@ const sources = ["https://media1.tenor.com/m/9e6n8-LUhH8AAAAd/oshi-no-ko-arima.g
                  "https://media1.tenor.com/m/BQ2eu8wKohgAAAAd/oshi-no-ko-onk.gif",
                  "https://media1.tenor.com/m/ybN0cfnFteIAAAAd/kana-flustered.gif"];
 
+function hasPassed(lastTime, minPassed)
+{
+    return new Date() - new Date(lastTime) >= minPassed;
+}
+
 function randint(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
-function waitForList(selector, times) {
-    let step = times ? 1 : 0;
-    times = times ? times : 10;
-    return new Promise((resolve) => {
-        let interval;
-        const testFunction = () => {
-            const list = document.querySelectorAll(selector);
-            if (list.length != 0 || times <= 0) {
-                clearInterval(interval);
-                resolve(list);
-            }
-            times -= step;
-        };
-        testFunction();
-        interval = setInterval(testFunction, 100);
-    });
-}
-
-async function addGIF()
+async function swapDisplay(Kana)
 {
-    const kanachan = document.createElement('img');
-    kanachan.src = sources[randint(0, sources.length)];
-    kanachan.style.cssText = `position: fixed; bottom: 10px; right: 20px; zIndex: 9999;
-    width: 200px; height: auto; opacity: 0.5; pointerEvents: none; display: ${await GM.getValue("display")};`;
-
-    (await waitForList("body"))[0].appendChild(kanachan);
-
-    document.addEventListener('keydown', async (e) => {
-        if (e.key == 'k' && e.altKey)
-        {
-            let status = await GM.getValue("display");
-            kanachan.style.display = status == 'block' ? 'none' : 'block';
-            await GM.setValue("display", status == 'block' ? 'none' : 'block');
-        }
-    });
-
-    window.onfocus = async function() {
-        kanachan.style.display = await GM.getValue("display");
-    };
+    let display = await GM.getValue("display");
+    Kana.style.display = display == 'block' ? 'none' : 'block';
+    await GM.setValue("display", display == 'block' ? 'none' : 'block');
+    await GM.setValue("lastAction", new Date().toISOString());
 }
 
-addGIF();
+
+async function addKana()
+{
+    let display = await GM.getValue("display");
+    let lastAction = await GM.getValue("lastAction");
+    if (display == "none" && hasPassed(lastAction, 1000 * 60 * 60 * 4))
+    {
+        display = "block";
+        await GM.setValue("display", display);
+    }
+
+    const Kana = document.createElement('img');
+    Kana.src = sources[randint(0, sources.length)];
+    Kana.style.cssText = `position: fixed; bottom: 10px; right: 20px; zIndex: 999999;
+    width: 200px; height: auto; opacity: 0.5; pointerEvents: none; display: ${display};`;
+    document.body.appendChild(Kana);
+
+    document.addEventListener('keydown', (e) => { if (e.key == 'k' && e.altKey) {swapDisplay(Kana)} });
+    window.onfocus = async () => {Kana.style.display = await GM.getValue("display")};
+}
+
+addKana();
